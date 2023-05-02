2da488d85858e9bbe669c8ca30b888f66ff9161b
Masatoshi Itoh <masatoshi9953@gmail.com>
2023-05-02 19:44:45 +0900

    added keycloak starter

---
+++ keycloak/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker run --name keycloak_dev -p 8180:8180 \
+        -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin \
+        quay.io/keycloak/keycloak:latest \
+        start-dev --http-port=8180
